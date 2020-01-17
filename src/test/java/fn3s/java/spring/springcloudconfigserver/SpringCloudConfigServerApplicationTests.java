4af621cf9c7d738af3e16a16381c89a556d9ae8d
Firmin Sosso Ngoutte <firmin.sossongoutte@soprasteria.com>
2020-01-17 10:23:58 +0100

    Initial Commit

---
+++ src/test/java/fn3s/java/spring/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fn3s.java.spring.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
